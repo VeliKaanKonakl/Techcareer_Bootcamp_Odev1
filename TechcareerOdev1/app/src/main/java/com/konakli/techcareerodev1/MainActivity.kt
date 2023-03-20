c831093f0f5e340600a6fd4a8f49e5bd87a08f3c
Veli Kaan Konaklı <konaklik@hotmail.com>
2023-03-21 00:32:07 +0300

    TechcareerOdev1
    Odev_1_Degiskenler

---
+++ TechcareerOdev1/app/src/main/java/com/konakli/techcareerodev1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.konakli.techcareerodev1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ TechcareerOdev1/app/src/main/java/com/konakli/techcareerodev1/odev1/TechcareerOdev1.kt
@@ -0,0 +1,55 @@
+package com.konakli.techcareerodev1.odev1
+
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.util.Date
+
+fun main() {
+
+    var sehir : String = "Adana"
+    var ulke : String = "Turkiye"
+    var telefon : String = "0(544)-888-9988"
+    var postaKodu : String = "80750"
+    var eMail : String = "konaklik@hotmail.com"
+    var meslek : String = "Android Developer"
+    var stokMiktari : Int = 554
+    var musteriAdi : String = "Mahmut"
+    var bakiye : Double = 293.55
+    var dogumGunu : String = "01-09-2000"
+    var maas : Double = 17500.00
+    var medeniDurum : String = "Bekar"
+    var urunYorum : String = "Bekledigimden daha iyi geldi. Hizli kargo"
+    var odemeTarihi : String = "07-04-2023"
+    var odeme : Double = 455.75
+    var siparisAdeti : Int = 33
+    var kitapAdi : String = "Santranc - Stefan Zweig"
+    var yayinlanmaTarihi : String = "1943"
+    var indirimMiktari : Int = 30
+    var odaSayisi : Int = 23
+    var enlem : String = "35 E 18"
+    var boylam : String = "37 N 01"
+    var urunAdi : String = "Keyboard"
+    var yemekFiyati : Double = 54.50
+    var marka : String = "Apple"
+    var muzikAdi : String = "Hekimoglu Turkusu"
+    var videoSuresi : Double = 4.34
+    var urunPuani : Double = 4.7
+    var resimAdi : String = "Manzara Resmi"
+    var dosyaFormati : String = "JPEG"
+    var renk : String = "Siyah"
+    var renkKodu : String = "#000000"
+    var telefonModeli : String = "Samsung Galaxy S22+"
+    var ekranBoyutu : Double = 6.1
+    var agirlik : Double = 192.5
+    var ulusalGun : String = "29 EKIM CUMHURIYET BAYRAMI"
+    var tatilGunu : String = "Pazar"
+    var rezervasyonTarihi : String = "01-07-2023"
+    var sokakAdi : String = "Menekse Sokak"
+    var otobusHatti : String = "Mesot-Bahceli Evler"
+    var kalanDakika : Int = 20
+    var takipKodu : Int = 234903124
+    var kuponSuresi : String = "1 AY"
+    var kuponKodu : String = "KA1621MART2023"
+    var faturaAdresi : String = "Ciftlikkoy mah. menekse sok. no: 23 kat:3 daireNo: 8 Mersin/Yenisehir"
+
+}
